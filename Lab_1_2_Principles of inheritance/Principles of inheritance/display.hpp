368291d267d11ed2bece53e36bbe798fb492f20d
shoodymon <shoody2016@yandex.ru>
2025-02-19 16:57:11 +0300

    ЛР 1-2
    Лаба из бэк

---
+++ Lab_1_2_Principles of inheritance/Principles of inheritance/display.hpp
@@ -0,0 +1,19 @@
+﻿#ifndef DISPLAY_HPP
+#define DISPLAY_HPP
+
+#include "management_system.hpp"
+
+void displayAllCarpets(CarpetItem* carpets[], int size) {
+    std::cout << "\n========= Информация о коврах =========" << std::endl;
+    for (int i = 0; i < size; i++) {
+        if (carpets[i] != nullptr) {
+            std::cout << "Ковер #" << (i + 1) << ":" << std::endl;
+            carpets[i]->showDetails();
+            std::cout << std::endl;
+        }
+    }
+    std::cout << "Общее количество ковров: " << CarpetItem::getTotalCarpets() << std::endl;
+    std::cout << "=======================================" << std::endl;
+}
+
+#endif // DISPLAY_HPP
